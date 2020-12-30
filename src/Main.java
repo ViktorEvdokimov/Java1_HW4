27010fe894311aa4dc1cf41ed9f363ce0beb5c3e
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2020-12-30 11:38:05 +0500

    Create field, print field

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	char field[][] = createField();
+	printField(field);
     }
+
+    static char[][] createField() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер поля");
+        int fieldSize = 1;
+        do {
+            fieldSize = sc.nextInt();
+            if (fieldSize <= 0) {
+                System.out.println("Размер поля должен быть больше 0");
+            }
+        }while (fieldSize<=0);
+        char [][]field = new char[fieldSize][fieldSize];
+        for (int i=0; i<field.length;i++){
+            for (int j=0;j<field.length; j++){
+                field[i][j] = '-';
+            }
+        }
+        return field;
+    }
+
+
+    static void printField (char [][]field){
+        for (int i=0; i<field.length;i++){
+            for (int j=0;j<field.length; j++){
+                System.out.print(field[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
